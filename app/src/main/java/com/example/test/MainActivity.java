d5cbd1b1eaff9fa15d8ada1637086b3b392bc116
Pradhaan-RK <pradhaanrkedlaya@gmail.com>
2023-04-23 19:57:57 +0530

    Commiting

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Button allowperm=findViewById(R.id.button);
+        allowperm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/test/MyAccessibilityService.java
@@ -0,0 +1,116 @@
+package com.example.test;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+public class MyAccessibilityService extends AccessibilityService {
+    int c=0;
+    private static final String TAG = "MyAccessibilityService";
+    @SuppressLint("SuspiciousIndentation")
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        //Log.e(TAG, "onAccessibilityEvent: ");
+            if(String.valueOf(event.getText()).contains("Notification")||String.valueOf(event.getText()).contains("Quick setting"))
+                Log.e(TAG,"Notification shade");
+        final int eventType = event.getEventType();
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+
+                Log.e(TAG, "Click");
+
+                AccessibilityNodeInfo sourceNode = event.getSource();
+
+                if (sourceNode != null) {
+                    Rect bounds = new Rect();
+                    sourceNode.getBoundsInScreen(bounds);
+                    int touchX = bounds.centerX();
+                    int touchY = bounds.centerY();
+                    Log.e(TAG, "Touch Location is X=" + touchX + " Y=" + touchY);
+                }
+                break;
+
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+
+                Log.e(TAG, "Long Click");
+                AccessibilityNodeInfo sourceNode2 = event.getSource();
+                if (sourceNode2 != null) {
+                    Rect bounds = new Rect();
+                    sourceNode2.getBoundsInScreen(bounds);
+                    int touchX = bounds.centerX();
+                    int touchY = bounds.centerY();
+                    Log.e(TAG, "Touch Location is X=" + touchX + " Y=" + touchY);
+                }
+                break;
+
+
+        }
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            AccessibilityNodeInfo rootNodeInfo = getRootInActiveWindow();
+            String packageName = event.getPackageName().toString();
+            PackageManager packageManager = this.getPackageManager();
+
+            if (packageName.equals("android")) {
+                Log.e(TAG, "The app has crashed");
+            } else {
+                try {
+                    ApplicationInfo applicationinfo = packageManager.getApplicationInfo(packageName, 0);
+                    CharSequence applicationlabel = packageManager.getApplicationLabel(applicationinfo);
+                    String windowTitle = event.getClassName().toString();
+                    if(windowTitle.equals("com.android.systemui.recents.RecentsActivity"))
+                            Log.e(TAG,"Recent Activity open");
+
+                    if (!applicationlabel.equals("Google App"))
+                        Log.e(TAG, "The app name is " + applicationlabel);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.e(TAG, "Name not found");
+                }
+            }
+        }
+
+    }
+
+        @Override
+        public void onInterrupt () {
+            Log.e(TAG, "onInterrupt: unexpected error");
+        }
+
+        @Override
+        protected void onServiceConnected () {
+            super.onServiceConnected();
+            AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+            info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+
+
+            info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+
+
+            info.notificationTimeout = 10;
+
+            this.setServiceInfo(info);
+            Log.e(TAG, "onServiceConnected: ");
+        }
+
+        private static void findPopups (AccessibilityNodeInfo nodeInfo){
+            if (nodeInfo.getChildCount() == 0) {
+                return;
+            }
+            for (int i = 0; i < nodeInfo.getChildCount(); i++) {
+                AccessibilityNodeInfo childNodeInfo = nodeInfo.getChild(i);
+                if (childNodeInfo.getClassName().equals("PopupWindow")){
+                    Log.e(TAG,"Popup detected");}
+
+                else {
+                    Log.e(TAG,childNodeInfo.getClassName().toString());
+                    findPopups(childNodeInfo);
+                }
+            }
+        }
+
+    }
