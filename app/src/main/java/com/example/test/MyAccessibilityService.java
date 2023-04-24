884362a773593a9d9fa3937a21ef3bf18d6a5b86
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-04-24 19:49:33 +0530

    Fine tune Java code with comments and code readability

--- app/src/main/java/com/example/test/MyAccessibilityService.java
+++ app/src/main/java/com/example/test/MyAccessibilityService.java
@@ -10,24 +10,30 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 public class MyAccessibilityService extends AccessibilityService {
-    int c=0;
+    int c = 0;
     private static final String TAG = "MyAccessibilityService";
-    @SuppressLint("SuspiciousIndentation")
+    @SuppressLint({"SuspiciousIndentation", "SwitchIntDef"})
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         //Log.e(TAG, "onAccessibilityEvent: ");t
-        String s=String.valueOf(event.getText());
-        if(!(s.contains("[]")) & !String.valueOf(event.getText()).contains("Notification"))
-            Log.i(TAG,s);
-            if(String.valueOf(event.getText()).contains("Notification")||String.valueOf(event.getText()).contains("Quick setting"))
-                Log.e(TAG,"Notification shade");
+
+        // ? Get the text of the event
+        String s = String.valueOf(event.getText());
+        if(!(s.contains("[]")) && !String.valueOf(event.getText()).contains("Notification"))
+            Log.i(TAG,s); // Print the text of the event (EG: [Home 1 of 1]
+        if(String.valueOf(event.getText()).contains("Notification")||String.valueOf(event.getText()).contains("Quick setting"))
+            Log.e(TAG,"Notification shade");
+
         final int eventType = event.getEventType();
         switch (eventType) {
+
+            // * Click event
             case AccessibilityEvent.TYPE_VIEW_CLICKED:
                 Log.i(TAG, "Click");
 
                 AccessibilityNodeInfo sourceNode = event.getSource();
 
+                // ? Get the location of the click
                 if (sourceNode != null) {
                     Rect bounds = new Rect();
                     sourceNode.getBoundsInScreen(bounds);
@@ -37,10 +43,13 @@ public class MyAccessibilityService extends AccessibilityService {
                 }
                 break;
 
+            // * Long click event
             case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
 
                 Log.i(TAG, "Long Click");
                 AccessibilityNodeInfo sourceNode2 = event.getSource();
+
+                // ? Get the location of the long click
                 if (sourceNode2 != null) {
                     Rect bounds = new Rect();
                     sourceNode2.getBoundsInScreen(bounds);
@@ -49,9 +58,9 @@ public class MyAccessibilityService extends AccessibilityService {
                     Log.i(TAG, "Touch Location is X=" + touchX + " Y=" + touchY);
                 }
                 break;
-
-
         }
+
+        // Detect if the app has crashed
         if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
 
             AccessibilityNodeInfo rootNodeInfo = getRootInActiveWindow();
@@ -62,44 +71,41 @@ public class MyAccessibilityService extends AccessibilityService {
                 Log.e(TAG, "The app has crashed");
             }
         }
-
     }
 
-        @Override
-        public void onInterrupt () {
-            Log.e(TAG, "onInterrupt: unexpected error");
-        }
-
-        @Override
-        protected void onServiceConnected () {
-            super.onServiceConnected();
-            AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-            info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-
-
-            info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
-
-
-            info.notificationTimeout = 10;
-
-            this.setServiceInfo(info);
-            Log.i(TAG, "onServiceConnected: ");
-        }
-
-        private static void findPopups (AccessibilityNodeInfo nodeInfo){
-            if (nodeInfo.getChildCount() == 0) {
-                return;
-            }
-            for (int i = 0; i < nodeInfo.getChildCount(); i++) {
-                AccessibilityNodeInfo childNodeInfo = nodeInfo.getChild(i);
-                if (childNodeInfo.getClassName().equals("PopupWindow")){
-                    Log.i(TAG,"Popup detected");}
-
-                else {
-                    Log.e(TAG,childNodeInfo.getClassName().toString());
-                    findPopups(childNodeInfo);
-                }
-            }
-        }
-
+    @Override
+    public void onInterrupt () {
+        Log.e(TAG, "onInterrupt: unexpected error");
     }
+
+    @Override
+    protected void onServiceConnected () {
+        super.onServiceConnected();
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+
+        info.notificationTimeout = 10;
+
+        this.setServiceInfo(info);
+        Log.i(TAG, "onServiceConnected: ");
+    }
+
+    private static void findPopups (AccessibilityNodeInfo nodeInfo){
+        if (nodeInfo.getChildCount() == 0) {
+            return;
+        }
+        for (int i = 0; i < nodeInfo.getChildCount(); i++) {
+            AccessibilityNodeInfo childNodeInfo = nodeInfo.getChild(i);
+            if (childNodeInfo.getClassName().equals("PopupWindow")) {
+                Log.i(TAG,"Popup detected");
+            }
+
+            else {
+                Log.e(TAG,childNodeInfo.getClassName().toString());
+                findPopups(childNodeInfo);
+            }
+        }
+    }
+}
